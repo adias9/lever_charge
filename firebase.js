4727c30804d214d964d374a73a3f34a8bc54398e
Dominic Whyte <dwhyte@princeton.edu>
2017-06-08 19:25:28 -0400

    test

--- firebase.js
+++ firebase.js
@@ -10,13 +10,36 @@ firebase.initializeApp({
 });
 
 
-var ref = firebase.database().ref("Updates");
+var ref = firebase.database().ref();
 ref.once("value")
   .then(function(snapshot) {
-  	console.log(JSON.stringify(snapshot));
+  	if (snapshot.hasChild("Users")) {
+  		console.log('has users')
+  		var users = snapshot.child('Users');
+  	}
+  	else {
+  		console.log('Error: no users');
+  	}
+
+  	if (snapshot.hasChild("Projects")) {
+  		console.log('has projects');
+  		var projects = snapshot.child('Projects');
+  		if (projects.hasChild('-KiqLms_JdnTo8d_fNZS')) {
+  			var title = projects.child('-KiqLms_JdnTo8d_fNZS').child('title');
+  			console.log('Title: ' + title.val());
+  		}
+  		else {
+  			console.log('Error: project DNE');
+  		}
+  	}
+  	else {
+  		console.log('Error: no projects');
+  	} 
+
+  	//console.log(JSON.stringify(snapshot));
+
+  	process.exit(0)
   });
-
-
 
 
 console.log('test end');
