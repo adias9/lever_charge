b379970862189a685499a8d41e08dd63e2b26710
Dominic Whyte <dwhyte@princeton.edu>
2017-06-07 22:57:29 -0400

    test

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ console.log('test start');
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("lever-60125-firebase-adminsdk-vk0qx-336375f9a7.json");
+var serviceAccount = require("./lever-60125-firebase-adminsdk-vk0qx-336375f9a7.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
