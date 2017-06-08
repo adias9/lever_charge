f989c37b60b1f4b3191a496a2866343f26ec999b
Dominic Whyte <dwhyte@princeton.edu>
2017-06-07 23:08:20 -0400

    test

--- firebase.js
+++ firebase.js
@@ -1,12 +1,22 @@
 console.log('test start');
 
-var admin = require("firebase-admin");
+var firebase = require("firebase-admin");
 
 var serviceAccount = require("./lever-60125-firebase-adminsdk-vk0qx-336375f9a7.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
   databaseURL: "https://lever-60125.firebaseio.com/"
 });
+
+
+var ref = firebase.database().ref("Updates");
+ref.once("value")
+  .then(function(snapshot) {
+  	console.log(JSON.stringify(snapshot));
+  });
+
+
+
 
 console.log('test end');
