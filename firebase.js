95f84e17ebc9295a20bb3e38843b6905c4894f20
Dominic Whyte <dwhyte@princeton.edu>
2017-06-07 22:52:54 -0400

    test

--- firebase.js
+++ firebase.js
@@ -2,10 +2,6 @@ console.log('test start');
 
 var admin = require("firebase-admin");
 
-import * as admin from "firebase-admin";
-
-var admin = require("firebase-admin");
-
 var serviceAccount = require("lever-60125-firebase-adminsdk-vk0qx-336375f9a7.json");
 
 admin.initializeApp({
@@ -13,3 +9,4 @@ admin.initializeApp({
   databaseURL: "https://lever-60125.firebaseio.com/"
 });
 
+console.log('test end');
